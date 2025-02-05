05c88b3ab655fb583ad5f8689ed74823e33f1d81
4lineclear <tamimhakimi21@gmail.com>
2025-02-05 16:27:12 -0500

    restarting.

--- crates/ui/src/lib.rs
+++ crates/ui/src/lib.rs
@@ -1,3 +1,6 @@
+use iced::widget::text_editor::{Action, Edit};
+use iced::widget::{text_editor, text_editor::Content};
+use iced::Length::Fill;
 use iced::{Element, Subscription, Task};
 
 // type Element<'a, T> = iced::Element<'a, T, iced::Theme, Renderer>;
@@ -5,64 +8,49 @@ use iced::{Element, Subscription, Task};
 
 pub type IceResult = iced::Result;
 
-pub mod editor;
-
 pub fn launch() -> IceResult {
     // todo!()
     iced::application("Editor - Iced", EvieMain::update, EvieMain::view)
         //  .theme(EvieMain::theme);
-        .subscription(EvieMain::subscription)
         // .default_font(Font::MONOSPACE)
         .run_with(EvieMain::new)
 }
 
+#[derive(Debug, Default)]
 struct EvieMain {
     // theme: highlighter::Theme,
+    content: Content,
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
     Idle,
+    Action(Action),
 }
 
 impl EvieMain {
     fn new() -> (Self, Task<Message>) {
         (
-            Self {},
+            Self::default(),
             // theme: highlighter::Theme::Base16Mocha,
             // Task::none(),
             iced::window::get_latest().and_then(|id| iced::window::maximize(id, true)),
         )
     }
 
-    fn subscription(&self) -> Subscription<Message> {
-        Subscription::none()
-        // event::listen_with(|event, _, _| match event {
-        //     iced::Event::Keyboard(event) => todo!(),
-        //     iced::Event::Mouse(event) => None,
-        //     iced::Event::Window(event) => None,
-        //     iced::Event::Touch(event) => None,
-        // })
-    }
-
-    fn update(&mut self, _message: Message) -> Task<Message> {
+    fn update(&mut self, message: Message) -> Task<Message> {
+        if let Message::Action(action) = message {
+            self.content.perform(action);
+        }
         Task::none()
-        // use keyboard::key::Named::*;
-        // match message {
-        // Message::KeyPress { key, mods } => match key {
-        //     Key::Named(named) => match named {
-        //         _ => todo!(),
-        //     },
-        //     Key::Character(_) => todo!(),
-        //     Key::Unidentified => todo!(),
-        // },
-        // Message::KeyRelease { key, mods } => todo!(),
-        // }
     }
     fn view(&self) -> Element<Message> {
         // |a| {println!("{a:#?}");
         // Message::Idle}
-        Element::new(editor::evie_editor())
+        text_editor(&self.content)
+            .height(Fill)
+            .on_action(Message::Action)
+            .into()
     }
 
     // fn theme(&self) -> Theme {
